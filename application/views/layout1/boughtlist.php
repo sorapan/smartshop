d1d68a00911b91c189cbf781376c4c9a9cc42b8e
Sorapan <t.sorapan@gmail.com>
2014-09-06 04:07:16 +0700

    6/09/14 4.06AM

---
+++ application/views/layout1/boughtlist.php
@@ -0,0 +1,120 @@
+<div class="bigheader">
+    รายการที่สั่งซื้อไว้แล้ว
+</div>
+
+<div class="col-xs-12 card">
+
+    <div class="cardbrand">รายการที่กำลังรอ</div>
+    <div class="col-xs-12 margintop">
+
+        <?php
+
+        if(count($wait_listdata)){
+
+        ?>
+
+        <table class="table">
+
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>วิธีส่ง</th>
+                <th>เงินที่ต้องจ่าย</th>
+                <th>เงินที่โอน</th>
+                <th>วันที่</th>
+                <th>เวลา</th>
+                <th>ภาพสลิป</th>
+                <th>สินค้าในตะกร้า</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            <?php
+            foreach($wait_listdata as $b_val){
+                ?>
+
+                <tr>
+                    <td><?=$b_val->id?> </td>
+                    <td class="hidden userid"><?=$b_val->userid?> </td>
+                    <td><?=$b_val->send=='none' ? 'รับด้วยตัวเอง' : 'ส่ง EMS' ?></td>
+                    <td><?=$b_val->price?> บาท</td>
+                    <td><?=$b_val->money?> บาท </td>
+                    <td><?=date("d.m.Y",$b_val->date)?> </td>
+                    <td><?=$b_val->time?> </td>
+                    <?php
+                    if($b_val->bill_dir !== NULL){
+                        ?>
+                        <td><a class="bill_img" data-toggle="modal" data-target="#myModal" www='bill_img/<?=$b_val->bill_dir?>'>คลิ๊ก</a></td>
+                    <?php
+                    }
+                    ?>
+                    <td>
+                        <a class="badge basket_detail" data-toggle="modal" data-target="#modal_basketdata">รายละเอียด</a>
+                    </td>
+
+                </tr>
+
+            <?php
+            }
+            }else{
+
+                echo '<h2 class="text-center text-grey marginbot">ข้อมูลว่างเปล่า</h2>';
+
+            }
+            ?>
+
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+
+<div class="col-xs-12 card margintop">
+    <div class="cardbrand">รายการที่แอดมินดำเนินการแล้ว</div>
+</div>
+
+<!--/////////////////////////////////////////////////-->
+
+<div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="addtobasket_name">ภาพสลิป</h4>
+            </div>
+
+            <div class="row">
+                <div class="modal-body">
+
+                </div>
+            </div>
+
+            <!--            <div class="modal-footer"></div>-->
+
+        </div>
+    </div>
+</div>
+
+<!--/////////////////////////////////////////////////-->
+
+<div class="modal" id="modal_basketdata" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="addtobasket_name">รายชื่อสินค้า</h4>
+            </div>
+
+            <div class="modal-body">
+            </div>
+
+            <!--            <div class="modal-footer"></div>-->
+
+        </div>
+    </div>
+</div>
---
+++ asset/js/boughtlist.js
@@ -0,0 +1,47 @@
+$(function(){
+
+    $(document).on('click','.bill_img',function(){
+
+        $(' .modal-body').html('' +
+            '<img class="marginbot col-xs-12" src="'+$(this).attr('www')+'"> '+
+            '')
+
+    });
+
+    $(document).on('click','.basket_detail',function(){
+
+        var display = $("#modal_basketdata").find(".modal-body");
+
+        display.html('');
+
+        display.append('<table class="table"><thead><tr>' +
+            '<th>ชื่อสินค้า</th>' +
+            '<th>จำนวน (ชิ้น)</th>' +
+            '<th>ราคา (บาท)</th>' +
+            '</tr></thead><tbody class="tabledata"></tbody></table>');
+
+        $.ajax({
+            url:'boughtlist/basketdetail',
+            type:'POST',
+            data:{
+                'userid' : $(this).parents().eq(2).find('.userid').html()
+            },
+            dataType:'JSON',
+            success:function(data){
+
+                for(var qwe in data){
+                    $(".tabledata").append("<tr>"+
+                        "<td>"+data[qwe]['productname']+"</td>"+
+                        "<td>"+data[qwe]['unit']+"</td>"+
+                        "<td>"+data[qwe]['price']+"</td>"+
+                        "</tr>");
+                }
+
+
+
+            }
+        });
+
+    });
+
+});
